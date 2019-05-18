1c7a0ae05b9efa45914387d6fe7894ba0042f088
kusaoisii <dkadd911@gmail.com>
2019-05-19 00:21:56 +0900

    first ci (may 19)

---
+++ chat/main.go
@@ -0,0 +1,26 @@
+
+package main
+
+import (
+"net/http"
+"log"
+)
+
+func main(){
+	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+			<html>
+				<head>
+                  <title>チャット<title>
+				</head>
+				<body>
+					チャットしましょう！
+				</body>
+			</html>
+	`))
+	})
+	if err := http.ListenAndServe(":8082",nil); err != nil {
+		log.Fatal("ListenAndServe",err)
+	}
+}
+
