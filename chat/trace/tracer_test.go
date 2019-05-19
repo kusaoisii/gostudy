11aca900db49261fde924452a4e2285c1d5bdb00
kusaoisii <dkadd911@gmail.com>
2019-05-19 21:45:17 +0900

    add trace

---
+++ chat/trace/tracer_test.go
@@ -0,0 +1,19 @@
+package trace
+
+import (
+	"testing"
+	"bytes"
+)
+
+func TestNew(t *testing.T){
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Newから戻り値がnilです")
+	} else {
+		tracer.Trace("こんにちは、traceパッケージ")
+		if buf.String() != "こんにちは、traceパッケージ\n" {
+			t.Errorf("'%s'という余った文字列が出力されました",buf.String())
+		}
+	}
+}
